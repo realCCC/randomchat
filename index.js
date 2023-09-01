eb7ead414cbf0cadcfc68e66b32c9823c407f4a9
realCCC <wpdls2821@gmail.com>
2023-09-01 15:42:44 +0900

    test

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', (socket) => {
-    socket.broadcast.emit('chat message', 'user connection...');
-    socket.on('chat message', function(msg){
+    socket.broadcast.emit('chat message', '님이 접속하였습니다...');
+
+    socket.on('chat message', (msg) => {
         io.emit('chat massage', msg);
     });
-    socket.on('disconnection', function(){
-        socket.broadcast.emit('chat message', 'user disconnection...');
+
+    socket.on('disconnect', function () {
+        socket.broadcast.emit('chat message', '님이 도망갔습니다...');
     })
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
     console.log('listening on*: 3000');
 });
